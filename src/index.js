b73958bbe04c8aea5a6a068469fb21e10d284279
Edric Emmanuel Sia <edric.sia@gmail.com>
2021-10-07 20:37:12 +0800

    Adjusted routing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter>
+        <HashRouter basename="/">
             <UserProvider>
                 <App />
             </UserProvider>
